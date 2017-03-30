5b3bf303f60c004787cf7d6e14611b75f2b66217
Unknown <evlen@qip.ru>
2017-03-30 20:16:01 +0300

    First alpha
    First alpha version. Full backend with basic functionality, still ugly front

--- app.py
+++ app.py
@@ -1,21 +1,53 @@
-from flask import Flask, render_template
+from flask import Flask, request, session, redirect, render_template, flash
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from database import Base, Posts
+from database import Base, Posts, Users
+from hashlib import sha256
 
 app = Flask(__name__)
-engine = create_engine()
+engine = create_engine("postgresql://postgres:litva@127.0.0.1:5432/postgres")
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind = engine)
-session = DBSession()
+session_db = DBSession()
+app.secret_key = 'G\x8f\xee[N\x1b\x9a\x19\x16\nN\x00\xb3\x11\x0f\x08\xbfi\xd8\x14\x03o:\xfa'
 
 @app.route('/')
 def index_page():
-    return render_template("index.html", posts=session.query(Posts).all())
+    return render_template("index.html", posts=session_db.query(Posts).all())
 
 @app.route('/posts/<int:post_id>')
 def posts_page(post_id):
-    return render_template("post.html", post=session.query(Posts).filter(Posts.id == post_id).first())
+    return render_template("post.html", post=session_db.query(Posts).filter(Posts.id == post_id).first())
+
+@app.route('/page_admin')
+def page_admin():
+    if (not session.get('logged_in')):
+        return redirect('/login')
+    return render_template("page_admin.html")
+
+@app.route('/add_post', methods=['POST'])
+def add_post():
+    new_post = Posts(request.form['post_title'], request.form['post_text'])
+    session_db.add(new_post)
+    session_db.commit()
+    flash("Post_added")
+    return redirect('/')
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if (request.method == 'GET'):
+        return render_template("login.html")
+    else:
+        if (session_db.query(Users).filter(Users.login == request.form['login'] and Users.password == sha256(request.form['password'])).first()):
+            session['logged_in'] = True
+            if (session_db.query(Users).filter(Users.login == request.form['login'] and Users.isadmin == True).first()):
+                return redirect('/page_admin')
+            else:
+                flash("You're not an admin, redirecting to the posts page")
+                return redirect("/")
+        else:
+            flash("Wrong login, check the credentials")
+            return redirect('/login')
 
 if ( __name__ == "__main__"):
     app.run(debug=True);
--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String, Text
+from sqlalchemy import Column, ForeignKey, Integer, String, Text, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -16,6 +16,24 @@ class Posts(Base):
     likes_number = Column(
     Integer, nullable=False)
 
-engine = create_engine ()
+    def __init__(self, title, post_text, likes_number = 0):
+        self.title = title
+        self.post_text = post_text
+        self.likes_number = likes_number
+
+class Users(Base):
+    __tablename__ = 'users'
+    name = Column(
+    String(32), nullable=False)
+    id = Column(
+    Integer, primary_key=True)
+    login = Column(
+    String(32), nullable=False)
+    password = Column(
+    String(64), nullable=False)
+    isadmin = Column(
+    Boolean, nullable=False)
+
+engine = create_engine ("postgresql://postgres:litva@127.0.0.1:5432/postgres")
 
 Base.metadata.create_all(engine)
